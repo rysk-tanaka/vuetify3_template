608d8e5fdf89eecf34c1e51a2f6bf2422015679c
rysk-tanaka <tanaka_rysk@rysk.jp>
2024-05-10 18:09:01 +0900

    Add LoadingOverlay component to App

---
+++ src/components/LoadingOverlay.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-overlay
+    v-model="isLoading"
+    :persistent="true"
+    class="align-center justify-center"
+  >
+    <v-progress-circular
+      indeterminate
+      color="primary"
+      size="64"
+    ></v-progress-circular>
+  </v-overlay>
+</template>
+
+<script setup lang="ts">
+const loadingStore = useLoadingStore()
+const isLoading = computed(() => loadingStore.loading)
+</script>
